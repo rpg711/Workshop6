d233c12334912d103ca473fb09d7f5153abd1819
Tony Gao <zijiangao@umass.edu>
2016-11-25 14:15:05 -0500

    facebookGotServed

--- client/app/server.js
+++ client/app/server.js
@@ -10,6 +10,39 @@ function emulateServerReturn(data, cb) {
   }, 4);
 }
 
+/**
+* Resolves a feed item. Internal to the server, since it's synchronous.
+*/
+function getFeedItemSync(feedItemId) {
+var feedItem = readDocument('feedItems', feedItemId);
+// Resolve 'like' counter.
+feedItem.likeCounter = feedItem.likeCounter.map((id) =>
+readDocument('users', id));
+// Assuming a StatusUpdate. If we had other types of
+// FeedItems in the DB, we would
+// need to check the type and have logic for each type.
+feedItem.contents.author = readDocument('users',
+feedItem.contents.author);
+// Resolve comment author.
+feedItem.comments.forEach((comment) => {
+comment.author = readDocument('users', comment.author);
+});
+return feedItem;
+}
+
+export function getFeedData(user, cb) {
+  var xhr = new XMLHttpRequest();
+
+  xhr.open('GET', '/user/4/feed');
+
+  xhr.setRequestHeader('Authorization', 'Bearer eyJpZCI6NH0=');
+
+  xhr.addEventListener('load', function() {
+    // Call the callback with the data.
+    cb(JSON.parse(xhr.responseText));
+});
+  xhr.send();
+}
 
 /**
  * Adds a new status update to the database.
