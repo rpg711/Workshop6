20d49dc39db52e25146a729802653023153afc52
Tony Gao <zijiangao@umass.edu>
2016-11-25 01:21:54 -0500

    express

---
+++ server/src/server.js
@@ -0,0 +1,33 @@
+var util = require('./util.js');
+var reverseString = util.reverseString;
+
+// Imports the express Node module.
+var express = require('express');
+var bodyParser = require('body-parser')
+
+
+// Creates an Express server.
+var app = express();
+
+app.use(bodyParser.text());
+
+// Defines what happens when it receives the `GET /` request
+app.get('/', function (req, res) {
+res.send('Hello World!');
+});
+
+// Handle POST /reverse [data]
+app.post('/reverse', function (req, res) {
+  if (typeof(req.body) === 'string') {
+    var reversed = reverseString(req.body);
+    res.send(reversed);
+  }
+  else {
+    res.status(400).end()
+  }
+});
+
+// Starts the server on port 3000!
+app.listen(3000, function () {
+console.log('Example app listening on port 3000!');
+});
